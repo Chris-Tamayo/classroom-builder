5694ae2ac3e5a23f10c97377057252ecab59594c
chris-codes-things <chris.codes.thingz@gmail.com>
2026-03-06 11:39:49 -0800

    Revert "Add prerender plugin"
    This reverts commit fc1564aff4bbb4bbd5ed1aea8ef893b05ae74c87, reversing
    changes made to e1e1400f62590b808aac088402c186e9e31177dd.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import prerender from "vite-plugin-prerender";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -14,20 +13,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-    mode === "production" &&
-      prerender({
-        staticDir: path.resolve(__dirname, "dist"),
-        routes: [
-          "/",
-          "/random-group-generator",
-          "/classroom-schedule-maker",
-          "/blog/randomly-group-students",
-        ],
-      }),
-  ].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
